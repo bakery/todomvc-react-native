70ad10b5bc5e1ec9c6cb992d7ba6da537f816d89
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2016-11-27 17:58:21 +0000

    clean up server bootstrap code

--- server/src/graphql/index.js
+++ server/src/graphql/index.js
@@ -1,11 +1,18 @@
 import bodyParser from 'body-parser';
 import parseGraphQLHTTP from 'parse-graphql-server';
+import { graphiqlExpress } from 'graphql-server-express';
 import schema from './schema';
 
 export default {
   setup(app) {
+    const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     app.use('/graphql', parseGraphQLHTTP({ schema }));
+
+    if (IS_DEVELOPMENT) {
+      app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+    }
   },
 };
